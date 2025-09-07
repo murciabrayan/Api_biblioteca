3c25516417381d8c16e69765118af38103b71dee
Brayan <branfer60@gmail.com>
2025-09-07 17:04:07 -0500

    Se agregaron los serializers del proyecto

--- api_biblioteca/api_app/serializers.py
+++ api_biblioteca/api_app/serializers.py
@@ -1,35 +1,39 @@
 from rest_framework import serializers
-from .models import Autor, Editorial
+from .models import Autor, Editorial, Libro, Prestamo, Miembro
 
 
-class Autorserializer(serializers.ModelSerializer):
+class AutorSerializer(serializers.ModelSerializer):
     class Meta:
         model = Autor
         fields = '__all__'
 
 
-
-class Libroserializer(serializers.ModelSerializer):
-    class Meta:
-        model = Libro
-        fields = '__all__'
-
-
-class Editorialserializer(serializers.ModelSerializer):
+class EditorialSerializer(serializers.ModelSerializer):
     class Meta:
         model = Editorial
         fields = '__all__'
 
 
+class LibroSerializer(serializers.ModelSerializer):
+    autor = AutorSerializer(read_only=True)          # muestra datos del autor
+    editorial = EditorialSerializer(read_only=True)
 
-class Prestamoserializer(serializers.ModelSerializer):
+    class Meta:
+        model = Libro
+        fields = '__all__'
+
+class MiembroSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Miembro
+        fields = '__all__'
+
+class PrestamoSerializer(serializers.ModelSerializer):
+    libro = LibroSerializer(read_only=True)      # anida la info del libro
+    miembro = MiembroSerializer(read_only=True)  # anida la info del miembro
+
     class Meta:
         model = Prestamo
         fields = '__all__'
 
 
-class Miembrolserializer(serializers.ModelSerializer):
-    class Meta:
-        model = Miembro
-        fields = '__all__'
         
